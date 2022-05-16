971b1908075e700094042f433857c75062d4f80a
xuxin14 <xuxin14@staff.sina.com.cn>
2022-05-16 18:51:39 +0800

    Feature: 初步支持地址粘贴

--- utils/AutoAddArticleTemplateScript.py
+++ utils/AutoAddArticleTemplateScript.py
@@ -1,9 +1,12 @@
 # 自动放置模板脚本
 # !/usr/bin/env python
 import os
+import re
 import shutil
-from sys import argv
 import zipfile
+from sys import argv
+
+import wget
 
 
 def traverse_dir(current_dir, deep=0):
@@ -42,47 +45,57 @@ def zip(file_path, dst_path):
 
 
 # script, from_file, to_file = argv
-script, version_number = argv
+script, download_url = argv
+# download_url = "http://mjs.sinaimg.cn//wap/project/snal_v2/7.3.63/index/index.php"
+# version_number = re.match("\d.\d.\d{2}", download_url)
+pattern = re.compile(r'\d.\d.\d{2}')
+version_number = pattern.findall(download_url)[0]
+print("地址是 %s ,版本号是 %s" % (download_url, version_number))
 # 模板根目录
 tempRoot = "/Users/xuxin14/Desktop/Temp"
 # 替换根目录
 projectRoot = "/Users/xuxin14/Desktop/SinaProjects/SinaNews的副本"
-print("执行拷贝=============>\n")
-print("模板目录是 " + tempRoot + ", 工程目录是 " + projectRoot + '\n')
-
-# 拷贝目录
+# 定义常量
 from_file = tempRoot + "/" + version_number + "/index"
 to_file = projectRoot + "/SinaNews/src/main/assets/article_v2"
-
-# 1.解压缩
-# 1.1 修改文件名
 php_name = tempRoot + "/" + version_number + "/index.php"
 zip_name = tempRoot + "/" + version_number + "/index.zip"
+# 拷贝目录
+
+# 1. 下载文件准备目录
+print("下载文件路径 %s" % php_name)
+print("模板目录是 " + tempRoot + ", 工程目录是 " + projectRoot + '\n')
+os.mkdir(tempRoot + "/" + version_number)
+wget.download(download_url, out=php_name)
+# 2.解压缩
+# 2.1 修改文件名
+
 os.rename(php_name, zip_name)
-# 1.2 解压文件
+# 2.2 解压文件
 zip(zip_name, from_file)
-# 2.复制文件
-# 2.1 整理文件 移除多余文件
+# 3.复制文件
+print("执行拷贝=============>\n")
+# 3.1 整理文件 移除多余文件
 need_remove_file_path1 = from_file + '/' + version_number
 need_remove_file_path2 = from_file + "/version.json"
-traverse_dir(from_file, 1)
+# traverse_dir(from_file, 1)
 os.remove(need_remove_file_path1)
 print("移除文件=============>" + version_number + '\n')
 os.remove(need_remove_file_path2)
 print("移除文件=============> version.json \n")
-traverse_dir(from_file, 1)
+# traverse_dir(from_file, 1)
 
-# 2.2 复制文件
+# 3.2 复制文件
 print("递归拷贝从 %s 到 %s" % (from_file, to_file) + '\n')
 shutil.copytree(from_file, to_file, dirs_exist_ok=True)
 print("<=============拷贝结束\n")
 
-# 3.git commit
-# 3.1 进入到指定目录
+# 4.git commit
+# 4.1 进入到指定目录
 os.chdir(projectRoot)
 print("进入目录 当前 :")
 os.system('pwd')
-# 3.2 commit
+# 4.2 commit
 os.system('git add .')
 os.system('git commit -m \"Feature: 升级正文Hb 模板 %s\"' % version_number)
 print("提交完毕")
