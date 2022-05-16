8559b169e65e7fdcfaaf3e0766c829bb40858047
xuxin14 <xuxin14@staff.sina.com.cn>
2022-05-16 18:58:56 +0800

    Fix:去掉无用代码

--- utils/AutoAddArticleTemplateScript.py
+++ utils/AutoAddArticleTemplateScript.py
@@ -9,33 +9,6 @@ from sys import argv
 import wget
 
 
-def traverse_dir(current_dir, deep=0):
-    """
-    current_dir: 绝对路径: "./"  或当前路径: 'D:/picture'
-    """
-    dir_list = os.listdir(current_dir)
-    # traverse folder first.
-    path_list, file_list = [], []
-    for dir in dir_list:
-        path = os.path.join(current_dir, dir)
-        if os.path.isdir(path):
-            path_list.append(dir)
-        else:
-            file_list.append(dir)
-    dir_list = path_list + file_list
-
-    # traverse all dir.
-    for dir in dir_list:
-        path = os.path.join(current_dir, dir)
-        if os.path.isdir(path):
-            # do something to this directory
-            print("\t" * deep, dir)
-            traverse_dir(path, deep + 1)
-        if os.path.isfile(path):
-            # do something to this file
-            print("\t" * deep, "|--", dir)
-
-
 def zip_file(file_path, dst_path):
     z_file = zipfile.ZipFile(file_path, "r")
     # ZipFile.namelist(): 获取ZIP文档内所有文件的名称列表
