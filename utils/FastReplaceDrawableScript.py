4be235f95db5e717d7228f32dd50c3c7ca985e56
xuxin14 <xuxin14@staff.sina.com>
2022-06-18 16:30:01 +0800

    Fix: 命名调整，支持循环

--- utils/FastReplaceDrawableScript.py
+++ utils/FastReplaceDrawableScript.py
@@ -2,12 +2,8 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 import os
-import re
 import shutil
-import zipfile
 from sys import argv
-
-import wget
 
 # region zsh示例
 ''' 
@@ -26,7 +22,18 @@ eg:
 > $[project_root]/ fad [drawable_name]
 '''
 
+
 # endregion
+
+
+class DrawableFolder:
+    src_file = ""
+    dst_file = ""
+
+    def __init__(self, src_file, dst_file):
+        self.src_file = src_file
+        self.dst_file = dst_file
+
 
 script, drawable_name, drawable_root, project_root = argv
 print("替换的图标是 %s " % drawable_name)
@@ -40,41 +47,58 @@ folder_3x = "drawable-xxhdpi"
 folder_3x_night = "drawable-night-xxhdpi"
 
 # 目录定义
-from_file = drawable_root
-to_file = project_root + "/SinaNews/src/main/res"
+src_file_root = drawable_root
+dst_file_root = project_root + "/SinaNews/src/main/res"
 
 # region xhdpi 尺寸定义
-from_file_2x = drawable_root + "/" + folder_2x
-to_file_2x = to_file + "/" + folder_2x
-from_file_2x_night = drawable_root + "/" + folder_2x
-to_file_2x_night = to_file + "/" + folder_2x
+src_file_2x = drawable_root + "/" + folder_2x
+dst_file_2x = dst_file_root + "/" + folder_2x
+src_file_2x_night = drawable_root + "/" + folder_2x_night
+dst_file_2x_night = dst_file_root + "/" + folder_2x_night
+
+src_file_3x = drawable_root + "/" + folder_3x
+dst_file_3x = dst_file_root + "/" + folder_3x
+src_file_3x_night = drawable_root + "/" + folder_3x_night
+dst_file_3x_night = dst_file_root + "/" + folder_3x_night
 # endregion
 
+
+drawableList = list[DrawableFolder]()
+drawableList.append(DrawableFolder(src_file_2x, dst_file_2x))
+drawableList.append(DrawableFolder(src_file_2x_night, dst_file_2x_night))
+drawableList.append(DrawableFolder(src_file_3x, dst_file_3x))
+drawableList.append(DrawableFolder(src_file_3x_night, dst_file_3x_night))
+
 # endregion
 
-# region 1. 重命名文件
-try:
-    file = os.listdir(from_file_2x)[0]
-    print("找到2x 文件 %s " % file)
-    original_name = from_file_2x + "/" + file
-    need_file_name = from_file_2x + "/" + drawable_name
-    dst_file_name = to_file_2x + "/" + drawable_name
-    print("路径打印 %s " % original_name)
-    print("路径打印目的地 %s " % need_file_name)
-    os.rename(original_name, need_file_name)
-except Exception as e:
-    print("not find file %s" % e)
-else:
-    # file = from_file_2x_night + "/" + os.listdir(from_file_2x_night)[0]
-    # os.rename(file, drawable_name)
-    # endregion
 
-    # region 2. 强制替换
+for i, folder in enumerate(drawableList):
+    # region 1. 重命名文件
     try:
-        shutil.copyfile(need_file_name, dst_file_name)
+        file = os.listdir(folder.src_file)[0]
+        print("找到文件 %s " % file)
+        original_name = folder.src_file + "/" + file
+        need_file_name = folder.src_file + "/" + drawable_name
+        dst_file_name = folder.dst_file + "/" + drawable_name
+        # print("路径打印 %s " % original_name)
+        # print("路径打印目的地 %s " % need_file_name)
+        os.rename(original_name, need_file_name)
+    except FileNotFoundError:
+        print("未找到文件相应目录文件 %s ,忽略" % folder.src_file)
+        continue
     except Exception as e:
-        print("copy failed %s" % e)
-    # shutil.copyfile(from_file_2x_night, to_file_2x_night)
+        print("未找到文件 %s" % e)
+        continue
     # endregion
+    # region 2. 强制替换
     else:
-        print("复制完毕")
+        try:
+            print("执行替换 %s" % dst_file_name)
+            shutil.copyfile(need_file_name, dst_file_name)
+        except Exception as e:
+            print("拷贝失败 %s" % e)
+            continue
+        else:
+            print("替换图标素材完毕")
+
+    # endregion
