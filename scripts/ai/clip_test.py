258c680c475d698b2d934ba7a6cea09d48a7a226
xuxin14 <xuxin14@staff.sina.com>
2023-11-01 12:08:47 +0800

    Feature(clip):添加英文翻译, 与原始映射对应

--- scripts/ai/clip_test.py
+++ scripts/ai/clip_test.py
@@ -1,30 +1,38 @@
 from PIL import Image
-
 from transformers import CLIPProcessor, CLIPModel
+from googletrans import Translator
 
 model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+translator = Translator()
+
+
+def translate_to_english(text):
+    translation = translator.translate(text, src='zh-CN', dest='en')
+    return translation.text
+
 
 if __name__ == '__main__':
-    # url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-    # image = Image.open(requests.get(url, stream=True).raw)
-    # image_path = "/Users/xuxin14/Documents/Backup/头像/训练/girl.jpeg"
-    image_path = "/Users/xuxin14/Documents/Backup/头像/训练/cat.jpeg"
+    image_path = "/Users/xuxin14/Documents/Backup/头像/训练/girl.jpeg"
+    # image_path = "/Users/xuxin14/Documents/Backup/头像/训练/cat.jpeg"
     image = Image.open(image_path)
 
-    descriptions = ["a photo of a boy", "a photo of a girl", "a photo of a person",
-                    "a photo of a dog", "a photo of a cat", "a photo of a animal"]
-    inputs = processor(text=descriptions, images=image, return_tensors="pt", padding=True)
+    descriptions = ["一只猫", "多只猫", "a photo of a boy", "a photo of a girl", "a photo of a dog",
+                    "a photo of a person", "一个人的照片"]
+
+    translated_descriptions = [translate_to_english(desc) if '一' in desc else desc for desc in
+                               descriptions]  # 简单检查是否为中文
+
+    inputs = processor(text=translated_descriptions, images=image, return_tensors="pt", padding=True)
     outputs = model(**inputs)
 
-    logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
-    probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
-    probabilities = probs.detach().numpy()[0]  # 将tensor转换为numpy数组
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1)
+    probabilities = probs.detach().numpy()[0]
 
-    # 将文本描述和它们的概率值结合
+    # 使用原始描述
     description_probs = list(zip(descriptions, probabilities))
 
-    # 按概率值从高到低排序
     sorted_description_probs = sorted(description_probs, key=lambda x: x[1], reverse=True)
 
     for desc, prob in sorted_description_probs:
