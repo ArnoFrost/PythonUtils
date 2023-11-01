de15d047b263ffa59662fe8643acf757f842e8a1
xuxin14 <xuxin14@staff.sina.com>
2023-11-01 14:04:43 +0800

    WIP(clip):测试搜索功能中

--- scripts/ai/clip_test.py
+++ scripts/ai/clip_test.py
@@ -1,7 +1,12 @@
-from PIL import Image
-from transformers import CLIPProcessor, CLIPModel
-from googletrans import Translator
+import os
+import pickle
 
+from PIL import Image
+from googletrans import Translator
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import CLIPProcessor, CLIPModel
+
+# 初始化模型处理器
 model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
 translator = Translator()
@@ -12,7 +17,55 @@ def translate_to_english(text):
     return translation.text
 
 
-if __name__ == '__main__':
+def get_image_vector(image_path):
+    image = Image.open(image_path)
+    inputs = processor(text=[], images=image, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    return outputs.image_embeds.squeeze().cpu().detach().numpy()
+
+
+def get_text_vector(text_description):
+    inputs = processor(text=text_description, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    return outputs.text_embeds.squeeze().cpu().detach().numpy()
+
+
+def save_image_vectors(directory_path, save_path):
+    image_vectors = {}
+
+    # 遍历指定目录下的所有图片
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".jpg") or filename.endswith(".png"):  # 可以根据需要添加更多图片格式
+            file_path = os.path.join(directory_path, filename)
+            vector = get_image_vector(file_path)
+            image_vectors[file_path] = vector.cpu().detach().numpy()
+
+    # 保存为pickle文件
+    with open(save_path, 'wb') as handle:
+        pickle.dump(image_vectors, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def search_images_by_text(text_description: list, vectors_path: str):
+    # 加载保存的向量数据
+    with open(vectors_path, 'rb') as handle:
+        saved_vectors = pickle.load(handle)
+
+    # 计算给定文本描述的向量
+    text_vector = get_text_vector(text_description)
+
+    # 计算相似度
+    similarities = {}
+    for image_path, image_vector in saved_vectors.items():
+        similarity = cosine_similarity([text_vector], [image_vector])[0][0]
+        similarities[image_path] = similarity
+
+    # 根据相似度排序
+    sorted_similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_similarities
+
+
+def demo1():
     image_path = "/Users/xuxin14/Documents/Backup/头像/训练/girl.jpeg"
     # image_path = "/Users/xuxin14/Documents/Backup/头像/训练/cat.jpeg"
     image = Image.open(image_path)
@@ -37,3 +90,19 @@ if __name__ == '__main__':
 
     for desc, prob in sorted_description_probs:
         print(f"{desc}: {prob:.4f}")
+
+
+if __name__ == '__main__':
+    # 定义目录:
+    directory_path = "/Users/xuxin14/Documents/Backup/头像/训练"
+    vectors_path = "/Users/xuxin14/Documents/Backup/头像/训练/vectors.pkl"
+    # 保存向量
+    save_image_vectors(directory_path, vectors_path)
+
+    # 搜索图片：
+    text_description = ["a photo of a cat"]
+    results = search_images_by_text(text_description, vectors_path)
+
+    # 打印最相似的5个图片
+    for path, score in results[:5]:
+        print(f"Image: {path}, Similarity: {score:.4f}")
