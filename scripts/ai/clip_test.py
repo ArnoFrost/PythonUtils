d7f34bf103c777edac4e3e3be577a1b8d5ec4d78
xuxin14 <xuxin14@staff.sina.com>
2023-11-01 14:56:44 +0800

    Feature(clip):接入搜索检索图片功能

--- scripts/ai/clip_test.py
+++ scripts/ai/clip_test.py
@@ -15,7 +15,7 @@ translator = Translator()
 
 def translate_to_english(text):
     translation = translator.translate(text, src='zh-CN', dest='en')
-    return translation.text
+    return translation[0].text
 
 
 def get_image_vector(image_path):
@@ -51,7 +51,7 @@ def save_image_vectors(directory_path: str):
     return db
 
 
-def search_images_by_text(text_description, db):
+def search_images_by_text(text_description: list[str], db):
     text_vector = get_text_vector(text_description)
     return db.search_vector(text_vector, k=len(db.file_paths))
 
@@ -59,36 +59,15 @@ def search_images_by_text(text_description, db):
 if __name__ == '__main__':
     directory_path = "/Users/xuxin14/Documents/Backup/头像/训练"
     db = save_image_vectors(directory_path)
+    topK = 5
 
-    text_description = ["a photo of a road"]
-    results = search_images_by_text(text_description, db)
+    text_description = ["长白山天池"]
+    # 进行翻译
+    translated_descriptions = translate_to_english(text_description)
+    print(translated_descriptions)
 
-    for path, score in results[:5]:
+    # 检索结果
+    results = search_images_by_text(translated_descriptions, db)
+
+    for path, score in results[:topK]:
         print(f"Image: {path}, Similarity: {score:.4f}")
-
-
-def demo1():
-    image_path = "/Users/xuxin14/Documents/Backup/头像/训练/girl.jpeg"
-    # image_path = "/Users/xuxin14/Documents/Backup/头像/训练/cat.jpeg"
-    image = Image.open(image_path)
-
-    descriptions = ["一只猫", "多只猫", "a photo of a boy", "a photo of a girl", "a photo of a dog",
-                    "a photo of a person", "一个人的照片"]
-
-    translated_descriptions = [translate_to_english(desc) if '一' in desc else desc for desc in
-                               descriptions]  # 简单检查是否为中文
-
-    inputs = processor(text=translated_descriptions, images=image, return_tensors="pt", padding=True)
-    outputs = model(**inputs)
-
-    logits_per_image = outputs.logits_per_image
-    probs = logits_per_image.softmax(dim=1)
-    probabilities = probs.detach().numpy()[0]
-
-    # 使用原始描述
-    description_probs = list(zip(descriptions, probabilities))
-
-    sorted_description_probs = sorted(description_probs, key=lambda x: x[1], reverse=True)
-
-    for desc, prob in sorted_description_probs:
-        print(f"{desc}: {prob:.4f}")
